5a1f47999bb0b883dca858755e344d0e67656b3a
albin2023 <94976030+Albin-mca@users.noreply.github.com>
2022-10-29 00:19:16 +0530

    Updated Contact.js

--- Contact/js/contact.js
+++ Contact/js/contact.js
@@ -1,1 +1,50 @@
+const $ = document;
+let container = $.querySelector("#con");
+let buttons = $.querySelectorAll("button");
 
+window.onload = () => {
+  data.forEach((newItem) => {
+    createItem(newItem);
+  });
+};
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    buttons.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    let items = data.filter((item) => item.type == e.target.innerText);
+    container.innerHTML = "";
+    items.forEach((newItem) => {
+      createItem(newItem);
+    });
+    if (e.target.innerText == "All") {
+      data.forEach((newItem) => {
+        createItem(newItem);
+      });
+    }
+  });
+});
+
+const createItem = ({ image, title, price, text }) => {
+  let temp = `<div class="item">
+  <div>
+  <img
+    src=${image}
+    alt="logo"
+  />
+</div>
+<div class="right">
+  <div class="tops">
+    <h4>${title}</h4>
+    <span class="price">$${price}</span>
+  </div>
+  <p>
+    ${text}
+  </p>
+</div>
+</div>`;
+
+  container.innerHTML += temp;
+};
